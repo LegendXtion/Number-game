e526fa298ca19eacf2d422cfb663fe566978875e
Shivam <58726448+LegendXtion@users.noreply.github.com>
2022-09-02 13:01:51 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+SHOW_WARNING = False
+POSSIBLE_GAMES = {3:1,4:1,5:1,6:1,7:2,8:2,9:2,10:2} # {Number:Position}
+DESCRIPTION = '''This game is developed to increase your brain focus. To play this game you need click on numbers from 1 to 25 if it's 5X5 game in ascending order and same applies to other games. At the end of game it will show you time taken by you to complete the game if you clicked in right order. Otherwise you will loose the game.
+	'''
+SHOW_NUMBER_AFTER_CLICK = False
